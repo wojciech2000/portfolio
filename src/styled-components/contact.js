13c4929aa6fecae05f62fa00bc028f8116ccba2a
Wojciech Antonik <antonik.wojciech2000@gmail.com>
2020-05-20 18:51:35 +0200

    Create mobile and tablet version for Contact.js

--- src/styled-components/contact.js
+++ src/styled-components/contact.js
@@ -13,16 +13,38 @@ export const ContactWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    @media (max-width: 1024px) and (orientation: portrait)
+    {
+        top: 45%;
+        width: 60vw;
+    }
+
+    @media (max-width: 432px) and (orientation: portrait)
+    {
+        width: 80vw;
+    }
+
 `
 
 export const ContactInformation = styled.div`
     width: 80%;
-    display: flex;
     color: white;
     font-size: .8rem;
     margin: 2% 0;
     display: flex;
     align-items: center;    
+
+    @media (max-width: 1024px) and (orientation: portrait)
+    {
+        font-size: 1.4rem;
+    }
+
+    @media (max-width: 432px) and (orientation: portrait)
+    {
+        font-size: 2rem;
+    }
+
 `
 
 export const Icon = styled.div`
@@ -30,6 +52,18 @@ export const Icon = styled.div`
     margin-right: 5%;
     display: flex;
     align-items: center;
+
+    @media (max-width: 1024px) and (orientation: portrait)
+    {
+        font-size: 2rem;
+    }
+
+    @media (max-width: 432px) and (orientation: portrait)
+    {
+        font-size: 2.8rem;
+    }
+
+
 `
 
 export const ContrastColor = styled.span`
@@ -39,4 +73,17 @@ export const ContrastColor = styled.span`
 export const GitHub = styled.img`
     margin-top: 35%;
     width: 3rem;
+
+    @media (max-width: 1024px) and (orientation: portrait)
+    {
+        margin-top: 40%;
+        width: 5rem;
+    }
+
+    @media (max-width: 432px) and (orientation: portrait)
+    {
+        margin-top: 45%;
+        width: 6rem;
+    }
+
 `
