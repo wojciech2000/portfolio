a0c3607452ae0ba22a44820f96e346cba8db159e
Wojciech Antonik <antonik.wojciech2000@gmail.com>
2020-07-17 16:52:03 +0200

    Updates

---
+++ precache-manifest.83a3fccc01a131680322ce51771ea086.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e045614e4fcb79599b8654b4130d5238",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d2c508d4dc725b76f0cf",
+    "url": "/portfolio/static/js/2.9b9e465e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.9b9e465e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de62d59c329b9c79979",
+    "url": "/portfolio/static/js/main.f652fd55.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "7117961be91385c8ddb4374f356318ef",
+    "url": "/portfolio/static/media/background.7117961b.jpg"
+  },
+  {
+    "revision": "05824ba11a4d12175261a28c5ba21523",
+    "url": "/portfolio/static/media/elimination_table-desktop.05824ba1.gif"
+  },
+  {
+    "revision": "898064c3e4be4a3bf56a12b79b077708",
+    "url": "/portfolio/static/media/elimination_table-mobile.898064c3.gif"
+  },
+  {
+    "revision": "6c05c5553be23f8d8dd7ecc93e066f54",
+    "url": "/portfolio/static/media/github-icon.6c05c555.svg"
+  },
+  {
+    "revision": "47c628d8a2ad0bdfdaa317390667b238",
+    "url": "/portfolio/static/media/me.47c628d8.jpg"
+  },
+  {
+    "revision": "fdf4369d38985f0360458b2033d21bfa",
+    "url": "/portfolio/static/media/portfolio-desktop.fdf4369d.gif"
+  },
+  {
+    "revision": "a981e035da2a2ccfd9277eb7d9c8302c",
+    "url": "/portfolio/static/media/portfolio-mobile.a981e035.gif"
+  },
+  {
+    "revision": "30465a370bdb88a62d722ffa4cc95622",
+    "url": "/portfolio/static/media/progresser-desktop.30465a37.gif"
+  },
+  {
+    "revision": "22f027118d4399710cd7b642eaf2a376",
+    "url": "/portfolio/static/media/progresser-mobile.22f02711.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bbad79ef4ef19243b2e563df8469494.js"
+  "/portfolio/precache-manifest.83a3fccc01a131680322ce51771ea086.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
