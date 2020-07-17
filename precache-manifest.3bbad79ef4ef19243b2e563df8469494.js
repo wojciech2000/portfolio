5b9934e33c908f47eec004e5d3e24e3425f5a893
Wojciech Antonik <antonik.wojciech2000@gmail.com>
2020-07-17 16:29:55 +0200

    Updates

---
+++ precache-manifest.3bbad79ef4ef19243b2e563df8469494.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0f3b2e6d955421a7fa5334d5d6adf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32ab2d6ad729b2371a39",
+    "url": "/static/js/2.dbf9fed7.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.dbf9fed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283fc17f2e3bd03dc047",
+    "url": "/static/js/main.c22e3291.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "7117961be91385c8ddb4374f356318ef",
+    "url": "/static/media/background.7117961b.jpg"
+  },
+  {
+    "revision": "05824ba11a4d12175261a28c5ba21523",
+    "url": "/static/media/elimination_table-desktop.05824ba1.gif"
+  },
+  {
+    "revision": "898064c3e4be4a3bf56a12b79b077708",
+    "url": "/static/media/elimination_table-mobile.898064c3.gif"
+  },
+  {
+    "revision": "6c05c5553be23f8d8dd7ecc93e066f54",
+    "url": "/static/media/github-icon.6c05c555.svg"
+  },
+  {
+    "revision": "47c628d8a2ad0bdfdaa317390667b238",
+    "url": "/static/media/me.47c628d8.jpg"
+  },
+  {
+    "revision": "fdf4369d38985f0360458b2033d21bfa",
+    "url": "/static/media/portfolio-desktop.fdf4369d.gif"
+  },
+  {
+    "revision": "a981e035da2a2ccfd9277eb7d9c8302c",
+    "url": "/static/media/portfolio-mobile.a981e035.gif"
+  },
+  {
+    "revision": "30465a370bdb88a62d722ffa4cc95622",
+    "url": "/static/media/progresser-desktop.30465a37.gif"
+  },
+  {
+    "revision": "22f027118d4399710cd7b642eaf2a376",
+    "url": "/static/media/progresser-mobile.22f02711.gif"
+  }
+]);
