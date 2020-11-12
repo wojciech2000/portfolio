8391ff900d7019fefbda9c40758c0e18182700df
Wojciech Antonik <wojtekantonik9@gmail.com>
2020-11-12 12:48:02 +0100

    add new skills, shorten name of technology in projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,81 +1,110 @@
-import React, { useContext } from 'react'
-import { DataContext } from './DataContext'
-import { ProjectsWrapper, Project, DesktopVersion, MobileVersion, ProjectInfo, UlList, LiList, Buttons, Button } from '../styled-components/projects'
-import progresserDesktop from '../pictures/progresser-desktop.gif'
-import progresserMobile from '../pictures/progresser-mobile.gif'
-import eliminationTableDesktop from '../pictures/elimination_table-desktop.gif'
-import eliminationTableMobile from '../pictures/elimination_table-mobile.gif'
-import portfolioDesktop from '../pictures/portfolio-desktop.gif'
-import portfolioMobile from '../pictures/portfolio-mobile.gif'
-import eCommerceDesktop from '../pictures/e-commerce-desktop.gif'
-import eCommerceMobile from '../pictures/e-commerce-mobile.gif'
+import React, {useContext} from "react";
+import {DataContext} from "./DataContext";
+import {
+  ProjectsWrapper,
+  Project,
+  DesktopVersion,
+  MobileVersion,
+  ProjectInfo,
+  UlList,
+  LiList,
+  Buttons,
+  Button,
+} from "../styled-components/projects";
+import progresserDesktop from "../pictures/progresser-desktop.gif";
+import progresserMobile from "../pictures/progresser-mobile.gif";
+import eliminationTableDesktop from "../pictures/elimination_table-desktop.gif";
+import eliminationTableMobile from "../pictures/elimination_table-mobile.gif";
+import portfolioDesktop from "../pictures/portfolio-desktop.gif";
+import portfolioMobile from "../pictures/portfolio-mobile.gif";
+import eCommerceDesktop from "../pictures/e-commerce-desktop.gif";
+import eCommerceMobile from "../pictures/e-commerce-mobile.gif";
 
 function Projects() {
+  const {pageVariants} = useContext(DataContext);
 
-    const { pageVariants } = useContext(DataContext)
+  const displayProjects = () => {
+    const projects = [
+      {
+        desktop: progresserDesktop,
+        mobile: progresserMobile,
+        usedTechnologies: [
+          "React",
+          "Redux",
+          "Node.js",
+          "Express",
+          "SCSS",
+          "MongoDB",
+        ],
+        githubLink: "https://github.com/wojciech2000/Progresser",
+        liveLink: "https://progresser-app.herokuapp.com/",
+      },
+      {
+        desktop: eliminationTableDesktop,
+        mobile: eliminationTableMobile,
+        usedTechnologies: ["HTML", "SCSS", "JS"],
+        githubLink: "https://github.com/wojciech2000/elimination-table",
+        liveLink: "https://wojciech2000.github.io/elimination-table/",
+      },
+      {
+        desktop: portfolioDesktop,
+        mobile: portfolioMobile,
+        usedTechnologies: ["React", "Styl.-Comp."],
+        githubLink: "https://github.com/wojciech2000/portfolio",
+      },
+      {
+        desktop: eCommerceDesktop,
+        mobile: eCommerceMobile,
+        usedTechnologies: [
+          "React",
+          "Redux",
+          "Node.js",
+          "Express",
+          "SCSS",
+          "MongoDB",
+        ],
+        githubLink: "https://github.com/wojciech2000/e-commerce-shop",
+        liveLink: "https://e-comerce-app.herokuapp.com/",
+      },
+    ];
 
-    const displayProjects = () => {
-
-        const projects = [
-            {
-                desktop: progresserDesktop,
-                mobile: progresserMobile,
-                usedTechnologies: ["React", "Redux", "Node.js", "Express", "SCSS", "MongoDB"],
-                githubLink: "https://github.com/wojciech2000/Progresser",
-                liveLink: "https://progresser-app.herokuapp.com/"
-            },
-            {
-                desktop: eliminationTableDesktop,
-                mobile: eliminationTableMobile,
-                usedTechnologies: ["HTML", "SCSS", "JS"],
-                githubLink: "https://github.com/wojciech2000/elimination-table",
-                liveLink: "https://wojciech2000.github.io/elimination-table/"
-            },
-            {
-                desktop: portfolioDesktop,
-                mobile: portfolioMobile,
-                usedTechnologies: ["React", "Styled-Components"],
-                githubLink: "https://github.com/wojciech2000/portfolio",
-            },
-            {
-                desktop: eCommerceDesktop,
-                mobile: eCommerceMobile,
-                usedTechnologies: ["React", "Redux", "Node.js", "Express", "SCSS", "MongoDB"],
-                githubLink: "https://github.com/wojciech2000/e-commerce-shop",
-                liveLink: "https://e-comerce-app.herokuapp.com/"
-            }
-        ]
-
-
-        return (projects.map(({ desktop, mobile, usedTechnologies, githubLink, liveLink  }, index) => (
-
-            <Project key={index}>
-            <DesktopVersion src={desktop} />
-            <MobileVersion src={mobile} />
-            <ProjectInfo>
-                <UlList>
-                    {usedTechnologies.map((technologie, index) => <LiList key={index}>{technologie}</LiList>)}
-                </UlList>
-                <Buttons portfolio={desktop === portfolioDesktop ? true : false}>
-                    <a href={githubLink} target="_blank" rel="noopener noreferrer"><Button code>Code</Button></a>
-                    {liveLink && <a href={liveLink} target="_blank" rel="noopener noreferrer"><Button>Live</Button></a>}
-                </Buttons>
-            </ProjectInfo>
+    return projects.map(
+      ({desktop, mobile, usedTechnologies, githubLink, liveLink}, index) => (
+        <Project key={index}>
+          <DesktopVersion src={desktop} />
+          <MobileVersion src={mobile} />
+          <ProjectInfo>
+            <UlList>
+              {usedTechnologies.map((technologie, index) => (
+                <LiList key={index}>{technologie}</LiList>
+              ))}
+            </UlList>
+            <Buttons portfolio={desktop === portfolioDesktop ? true : false}>
+              <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                <Button code>Code</Button>
+              </a>
+              {liveLink && (
+                <a href={liveLink} target="_blank" rel="noopener noreferrer">
+                  <Button>Live</Button>
+                </a>
+              )}
+            </Buttons>
+          </ProjectInfo>
         </Project>
+      ),
+    );
+  };
 
-        )) )
-
-    }
-
-    return (
-        <ProjectsWrapper
-        initial='in'
-        animate='done'
-        exit='out'
-        variants={pageVariants}>
-            {displayProjects()}
-        </ProjectsWrapper>
-    )
+  return (
+    <ProjectsWrapper
+      initial="in"
+      animate="done"
+      exit="out"
+      variants={pageVariants}
+    >
+      {displayProjects()}
+    </ProjectsWrapper>
+  );
 }
 
-export default Projects
+export default Projects;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,52 +1,68 @@
-import React, { useContext } from 'react'
-import { DataContext } from './DataContext'
-import { SkillsWrapper, SkillColumn, SklillIcon, SkillTitle, SklillNames, ContrastColor } from '../styled-components/skills'
-import { MdLaptopMac } from 'react-icons/md'
-import { FaDatabase } from 'react-icons/fa'
-import { FiSettings } from 'react-icons/fi'
+import React, {useContext} from "react";
+import {DataContext} from "./DataContext";
+import {
+  SkillsWrapper,
+  SkillColumn,
+  SklillIcon,
+  SkillTitle,
+  SklillNames,
+  ContrastColor,
+} from "../styled-components/skills";
+import {MdLaptopMac} from "react-icons/md";
+import {FaDatabase} from "react-icons/fa";
+import {FiSettings} from "react-icons/fi";
 function Skills() {
+  const {pageVariants} = useContext(DataContext);
 
-    const { pageVariants } = useContext(DataContext)
-
-    return (
-        <SkillsWrapper
-        initial='in'
-        animate='done'
-        exit='out'
-        variants={pageVariants}>
-            <SkillColumn>
-                <SklillIcon><MdLaptopMac /></SklillIcon>
-                <SkillTitle>Front-end</SkillTitle>
-                <SklillNames>
-                    <li>HTML5</li>
-                    <ContrastColor>CSS3</ContrastColor>
-                    <li>SCSS</li>
-                    <li>JAVASCRIPT</li>
-                    <li>REACT</li>
-                    <ContrastColor>REDUX</ContrastColor>
-                    <li>STYLED-COMPONENTS</li>
-                </SklillNames>
-            </SkillColumn>
-            <SkillColumn>
-                <SklillIcon><FaDatabase /></SklillIcon>
-                <SkillTitle>Back-end</SkillTitle>
-                <SklillNames>
-                    <li>NODE.JS</li>
-                    <li>EXPRESS</li>
-                    <ContrastColor>MYSQL</ContrastColor>
-                </SklillNames>
-            </SkillColumn>
-            <SkillColumn>
-                <SklillIcon><FiSettings /></SklillIcon>
-                <SkillTitle>Tools</SkillTitle>
-                <SklillNames>
-                    <ContrastColor>GIT / GITHUB</ContrastColor>
-                    <li>FIGMA</li>
-                    <li>NPM</li>
-                </SklillNames>
-            </SkillColumn>
-        </SkillsWrapper>
-    )
+  return (
+    <SkillsWrapper
+      initial="in"
+      animate="done"
+      exit="out"
+      variants={pageVariants}
+    >
+      <SkillColumn>
+        <SklillIcon>
+          <MdLaptopMac />
+        </SklillIcon>
+        <SkillTitle>Front-end</SkillTitle>
+        <SklillNames>
+          <li>HTML5</li>
+          <ContrastColor>CSS3</ContrastColor>
+          <li>SCSS</li>
+          <li>JAVASCRIPT</li>
+          <li>TYPESCRIPT</li>
+          <li>REACT</li>
+          <ContrastColor>REDUX</ContrastColor>
+          <li>STYLED-COMPONENTS</li>
+        </SklillNames>
+      </SkillColumn>
+      <SkillColumn>
+        <SklillIcon>
+          <FaDatabase />
+        </SklillIcon>
+        <SkillTitle>Back-end</SkillTitle>
+        <SklillNames>
+          <li>NODE.JS</li>
+          <li>EXPRESS</li>
+          <ContrastColor>MYSQL</ContrastColor>
+          <li>GRAPHQL</li>
+          <li>REST API</li>
+        </SklillNames>
+      </SkillColumn>
+      <SkillColumn>
+        <SklillIcon>
+          <FiSettings />
+        </SklillIcon>
+        <SkillTitle>Tools</SkillTitle>
+        <SklillNames>
+          <ContrastColor>NPM</ContrastColor>
+          <li>FIGMA</li>
+          <li>GIT</li>
+        </SklillNames>
+      </SkillColumn>
+    </SkillsWrapper>
+  );
 }
 
-export default Skills
+export default Skills;
