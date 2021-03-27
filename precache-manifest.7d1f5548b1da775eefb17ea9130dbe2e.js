a72c00da2d9b89c9bb413ed3a8aeb905315043fe
Wojciech Antonik <wojtekantonik9@gmail.com>
2021-03-27 16:34:21 +0100

    Updates

---
+++ precache-manifest.7d1f5548b1da775eefb17ea9130dbe2e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548d011d498cb078596a4e5461e3ce3d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e62d85c47b2b59aa6893",
+    "url": "/portfolio/static/js/2.dd39aa1a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.dd39aa1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d1d3c9a1535b629d13",
+    "url": "/portfolio/static/js/main.b787d801.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "7117961be91385c8ddb4374f356318ef",
+    "url": "/portfolio/static/media/background.7117961b.jpg"
+  },
+  {
+    "revision": "4365460c438ef93e7c151ce8dd6daa3a",
+    "url": "/portfolio/static/media/e-commerce-desktop.4365460c.png"
+  },
+  {
+    "revision": "c7c802bba94656779d7c9c47c424272d",
+    "url": "/portfolio/static/media/e-commerce-mobile.c7c802bb.png"
+  },
+  {
+    "revision": "6e62895557c1f8e57a6871048fffbaed",
+    "url": "/portfolio/static/media/elimination_table-desktop.6e628955.png"
+  },
+  {
+    "revision": "0d08981de019bacaa208bbd0c414207c",
+    "url": "/portfolio/static/media/elimination_table-mobile.0d08981d.png"
+  },
+  {
+    "revision": "6c05c5553be23f8d8dd7ecc93e066f54",
+    "url": "/portfolio/static/media/github-icon.6c05c555.svg"
+  },
+  {
+    "revision": "47c628d8a2ad0bdfdaa317390667b238",
+    "url": "/portfolio/static/media/me.47c628d8.jpg"
+  },
+  {
+    "revision": "02e5ec97e78907b0002cdd5d859c90fa",
+    "url": "/portfolio/static/media/portfolio-desktop.02e5ec97.png"
+  },
+  {
+    "revision": "e5cba177348ca536c60bf50975bc8cc2",
+    "url": "/portfolio/static/media/portfolio-mobile.e5cba177.png"
+  },
+  {
+    "revision": "165a12292d790c13f6d5efc72800c859",
+    "url": "/portfolio/static/media/progresser-desktop.165a1229.png"
+  },
+  {
+    "revision": "446b1e0adcbba975f039639fdaf5817c",
+    "url": "/portfolio/static/media/progresser-mobile.446b1e0a.png"
+  },
+  {
+    "revision": "80b7f1cd11519f3b62ec3b66fdce31b6",
+    "url": "/portfolio/static/media/search-images-desktop.80b7f1cd.png"
+  },
+  {
+    "revision": "1ef3f8b0238a1baff792cbd2b56547e0",
+    "url": "/portfolio/static/media/search-images-mobile.1ef3f8b0.png"
+  },
+  {
+    "revision": "7e857e0a8b8f1f645a716b8285ac4050",
+    "url": "/portfolio/static/media/team-mate-finder-desktop.7e857e0a.png"
+  },
+  {
+    "revision": "d1d9032d6846b4667158ee0ea28949d2",
+    "url": "/portfolio/static/media/team-mate-finder-mobile.d1d9032d.png"
+  }
+]);
